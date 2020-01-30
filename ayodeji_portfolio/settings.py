e84c4c0029e72ba728259fcaa587aadd7b43225a
Ayodeji <ayodejiayankola@gmail.com>
2020-01-30 14:44:26 +0100

    fixes to static files

--- ayodeji_portfolio/settings.py
+++ ayodeji_portfolio/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/images/'
 
@@ -129,3 +129,7 @@ STATICFILES_DIRS= [
     os.path.join(BASE_DIR, 'staticfiles')
 
 ]
+
+
+MEDIA_ROOT =   os.path.join(BASE_DIR, 'staticfiles/images')
+
